8da29bd658ba9768d39ed121e3fe41ca329f7852
Marcusjb87 <93656800+Marcusjb87@users.noreply.github.com>
2023-02-02 11:30:04 -0300

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,3 +1,14 @@
+// Selectores
+
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+
+const botonAbrir = document.getElementById('boton-carrito')
+
+const botonCerrar = document.getElementById('carritoCerrar')
+
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+
+
 // Eventos
 
 botonAbrir.addEventListener('click', ()=>{
